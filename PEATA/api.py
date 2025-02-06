21c8b5b50c0e220f406c7e3bea08b7316bb1e87c
amalie246 <amni002@egms.no>
2025-02-06 14:24:11 +0100

    loading dotenv and os

--- PEATA/api.py
+++ PEATA/api.py
@@ -1,1 +1,10 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BASE_URL = os.getenv("BASE_URL")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_KEY = os.getenv("CLIENT_KEY")
