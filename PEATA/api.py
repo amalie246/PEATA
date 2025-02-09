99fc294b99f76522ee544575c4ebaf637d1f46b7
amalie246 <amni002@egms.no>
2025-02-09 16:51:03 +0100

    created some requests tests on open source api

--- PEATA/api.py
+++ PEATA/api.py
@@ -8,3 +8,16 @@ BASE_URL = os.getenv("BASE_URL")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_KEY = os.getenv("CLIENT_KEY")
+
+#Testing connecting to another open-source API
+response = requests.get("https://api.github.com")
+
+#Status code
+print("Response: %d" % response.status_code)
+
+#Response content, JSON format
+print(response.content)
+
+#Getting specific headers
+print("Content type: ", end="")
+print(response.headers["Content-Type"])
