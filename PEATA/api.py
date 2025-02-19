b47241ee9a7b625ef6c5af98f9665ccddd559518
amalie246 <amni002@egms.no>
2025-02-19 20:01:24 +0100

    refactored to TikTokApi class

--- PEATA/api.py
+++ PEATA/api.py
@@ -4,32 +4,37 @@ import os
 
 dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
 load_dotenv(dotenv_path=dotenv_path)
+#access_token = obtain_access_token(CLIENT_KEY, CLIENT_SECRET)
+#print(access_token)
 
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-CLIENT_KEY = os.getenv("CLIENT_KEY")
-ENDPOINT_URL = "https://open.tiktokapis.com/v2/oauth/token/"
-
-#Obtain a client access token, add this to the authorization header
-def obtain_access_token(client_key, client_secret):
-    access_token_headers = {'Content-Type' : 'application/x-www-form-urlencoded', 
-                            'Cache-Control' : 'no-cache'}
-    req_body_params = {
-        'client_key': CLIENT_KEY,
-        'client_secret': CLIENT_SECRET,
-        'grant_type': 'client_credentials'
-    }
-
-    response = requests.post(ENDPOINT_URL, headers=access_token_headers, data=req_body_params)
+class TikTokApi:
     
-    if response.status_code == 200:
-        print(response.status_code)
-        print(response.json())
+    def __init__(self):
+        self.client_key = os.getenv("CLIENT_KEY")
+        self.client_secret = os.getenv("CLIENT_SECRET")
+        self.access_token = self.obtain_access_token()
+
+    #Obtain a client access token, add this to the authorization header
+    def obtain_access_token(self):
+        ENDPOINT_URL = "https://open.tiktokapis.com/v2/oauth/token/"
+        access_token_headers = {'Content-Type' : 'application/x-www-form-urlencoded', 
+                                'Cache-Control' : 'no-cache'}
+        req_body_params = {
+            'client_key': self.client_key,
+            'client_secret': self.client_secret,
+            'grant_type': 'client_credentials'
+        }
+
+        response = requests.post(ENDPOINT_URL, headers=access_token_headers, data=req_body_params)
         
-        json_resp = response.json();
-        return 0; #return json_resp['access_token']
-    else:
-        print("Something went wrong")
-        return 0;
-  
-access_token = obtain_access_token(CLIENT_KEY, CLIENT_SECRET)
-print(access_token)
+        if response.status_code == 200:
+            print(response.status_code)
+            print(response.json())
+            
+            json_resp = response.json();
+            return 0; #return json_resp['access_token']
+        else:
+            print("Something went wrong")
+            return 0;
+tiktok = TikTokApi()
+access_token = tiktok.obtain_access_token()
