c30d4d77f284645e40a06fc2a2906110a57f3fe1
amalie246 <amni002@egms.no>
2025-02-25 20:30:12 +0100

    created some TODOs and an extra file

---
+++ PEATA/queryFormatter.py
@@ -0,0 +1,6 @@
+#Handle query body formatting so user can easily pick filterings for which data
+#Also coordinate video retrieval with user and comment informations retrieval!
+
+#get_public_user info only needs to be called once
+#get_video_comments needs to be called once per video retrieved! 
+#This may cause problems to the API
