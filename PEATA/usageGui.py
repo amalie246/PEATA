a61a6c2e9d57cc12e6f744971b970a999849770a
amalie246 <amni002@egms.no>
2025-03-07 16:01:07 +0100

    created a dropdown menu

--- PEATA/usageGui.py
+++ PEATA/usageGui.py
@@ -9,9 +9,29 @@ class Gui:
         self.client_key = ck
     
     def page(self):
+        boolean_operations = ["and", "or", "not"]
+        #LT/LTE/GT/GTE must be used for date i think
+        #And i think IN is in a gange of some sort
+        eq_operations = ["EQ", "IN", "LT", "LTE", "GT", "GTE"]
+        video_fields = []
+        user_info_fields = []
+        comment_fields = []
+        
         root = tk.Tk()
         root.title("Test Box")
         root.geometry("300x200")
+        def show_selection():
+            selected_option = dropdown_boolean_operations.get()
+        
+        dropdown_boolean_operations = tk.StringVar(root)
+        #sdropdown_boolean_operations.set[boolean_operations[0]]
+        
+        dropdown_boolean = tk.OptionMenu(root, dropdown_boolean_operations, *boolean_operations)
+        dropdown_boolean.pack(pady=30)
+        
+        boolean_operations_btn = tk.Button(root, text="Show bool operations", command=show_selection)
+        boolean_operations_btn.pack()
+        
         
         def show_popup():
             messagebox.showinfo("Information", "This is a popup box!")
