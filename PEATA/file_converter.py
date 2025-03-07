f08623818243cadc3fb32bfdae5c0a8e6a3f0353
amalie246 <amni002@egms.no>
2025-03-07 12:41:46 +0100

    created main entry point for the project

--- PEATA/file_converter.py
+++ PEATA/file_converter.py
@@ -1,32 +1,34 @@
 import json
 import csv
 
-def save_json_to_file(data, filename="data.json"):
-    with open(filename, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4, ensure_ascii=False)
-    print(f"JSON-data lagret i {filename}")
+class FileConverter:
+    def save_json_to_file(data, filename="data.json"):
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+        print(f"JSON-data lagret i {filename}")
 
-def save_json_to_csv(data, filename="data.csv"):
-    if not data or not isinstance(data, list) or not isinstance(data[0], dict):
-        print("Ingen gyldige data å lagre.")
-        return
-    
-    fieldnames = set()
-    for item in data:
-        fieldnames.update(item.keys())
-
-    with open(filename, mode="w", newline="", encoding="utf-8") as file:
-        writer = csv.DictWriter(file, fieldnames=sorted(fieldnames))
-        writer.writeheader()
+    def save_json_to_csv(self, data, filename="data.csv"):
+        if not data or not isinstance(data, list) or not isinstance(data[0], dict):
+            print("Ingen gyldige data å lagre.")
+            return
+        
+        fieldnames = set()
         for item in data:
-            writer.writerow(item) 
+            fieldnames.update(item.keys())
 
-    print(f"JSON-data lagret i CSV-fil: {filename}")
+        with open(filename, mode="w", newline="", encoding="utf-8") as file:
+            writer = csv.DictWriter(file, fieldnames=sorted(fieldnames))
+            writer.writeheader()
+            for item in data:
+                writer.writerow(item) 
+
+        print(f"JSON-data lagret i CSV-fil: {filename}")
 
 
-def save_any_json_data(data, filename="output", file_format="json"):
-    filename = f"{filename}.{file_format}"
-    if file_format == "json":
-        save_json_to_file(data, filename)
-    elif file_format == "csv":
-        save_json_to_csv(data, filename)
+    def save_any_json_data(self, data, filename="output", file_format="json"):
+        filename = f"{filename}.{file_format}"
+        if file_format == "json":
+            self.save_json_to_file(data, filename)
+        elif file_format == "csv":
+            self.save_json_to_csv(data, filename)
+
---
+++ PEATA/main.py
@@ -0,0 +1,16 @@
+from api import TikTokApi
+from queryFormatter import QueryFormatter
+from file_converter import FileConverter
+#from fileHandler import FileHandler
+
+def main():
+    tiktok_api = TikTokApi();
+    query_formatter = QueryFormatter();
+    file_converter = FileConverter();
+    #file_handler = FileHandler();
+    
+    tiktok_api.get_video_comments("7463699433146961194");
+    
+
+if __name__ == "__main__":
+    main()
