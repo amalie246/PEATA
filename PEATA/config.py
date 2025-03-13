340cc6e4c46dca110ea356b329f96475a5b45565
ElinEunjung <eunjungp615@gmail.com>
2025-03-13 13:44:07 +0100

    Modified folder structure (data/json, data/csv, data/exports), fileConverter.py, fileHandler.py. Added config.py, Deleted dummy files

---
+++ PEATA/config.py
@@ -0,0 +1,13 @@
+import os
+
+BASE_FOLDER = "data"
+JSON_FOLDER = os.path.join(BASE_FOLDER, "json")
+CSV_FOLDER = os.path.join(BASE_FOLDER, "csv")
+EXPORTS_FOLDER = os.path.join(BASE_FOLDER, "exports")
+
+# Ensure folders exist
+os.makedirs(JSON_FOLDER, exist_ok=True)
+os.makedirs(CSV_FOLDER, exist_ok=True)
+os.makedirs(EXPORTS_FOLDER, exist_ok=True)
+
+
