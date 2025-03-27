5ff914ec9943d03d730a268047791434878bb3d0
odanostdahl <oda.nostdahl@gmail.com>
2025-03-27 12:03:23 +0100

    trying to test fileProcessor

--- PEATA/main.py
+++ PEATA/main.py
@@ -1,7 +1,6 @@
 from api import TikTokApi
 from queryFormatter import QueryFormatter
-from file_converter import FileConverter
-#from fileHandler import FileHandler
+from FileProcessor import FileProcessor
 #from tiktok_login import Login
 from gui import Gui
 from tkinter import Tk
@@ -15,31 +14,53 @@ def main():
     #    print("Access token could not be obtained. Exiting program.")
     #    return
     
-    gui = Gui("a", "b", "c", "d")
-    gui.test_page()
+    #gui = Gui("a", "b", "c", "d")
+    #gui.test_page()
+    
+    tiktok_api = TikTokApi()
+    
+    username = "izzyandmarysdad"
+    keyword = "Chicken"
+    start_date = "20250310"
+    end_date = "20250318"
 
     
     
-    #videos = tiktok_api.get_videos("izzyandmarysdad", "keyword", "20250310", "20250318")
+    videos = tiktok_api.get_videos(username, keyword, start_date, end_date)
     #Can check if a video query didnt go well by checking if not videos
     #Nothing to download, tell user that they need different arguments
-    #if not videos:
-    #    print("Did not retrieve videos")
-    #query_formatter = QueryFormatter()
-    #t1 = ("username", "izzyandmarysdad", "EQ")
-    #t2 = ("keyword", "Chicken", "EQ")
-    #args = [t1]
-    #and_clause = query_formatter.query_AND_clause(args)
+    if not videos:
+     print("Did not retrieve videos")
+     return
+ 
+    print(f"Fetched videos: {videos}")
     
+    query_formatter = QueryFormatter()
+    t1 = ("username", username, "EQ")
+    t2 = ("keyword", keyword, "EQ")
+    args = [t1, t2]
+    and_clause = query_formatter.query_AND_clause(args)
+    print(f"Formatted query: {and_clause}")
+    
+  
+    file_processor = FileProcessor()
+    file_processor.save_any_json_data(videos, filename="tiktok_videos", file_format="json")
+    file_processor.save_any_json_data(videos, filename="tiktok_videos", file_format="csv")
+    
+    data = file_processor.open_file()
+    
+    if data is not None:
+        file_processor.export_as_excel()
+        
+        file_processor.close_file()
 
-    # videos = tiktok_api.get_videos("izzyandmarysdad", "keyword", "20250310", "20250318")
+    #videos = tiktok_api.get_videos("izzyandmarysdad", "keyword", "20250310", "20250318")
     #Can check if a video query didnt go well by checking if not videos
     #Nothing to download, tell user that they need different arguments
     # if not videos:
         # print("Did not retrieve videos")
     #query_formatter = QueryFormatter()
-    #file_converter = FileConverter()
-    #file_handler = FileHandler()
+  
     
     #In login, check if client secret stuff are valid by fetching access token
     #root = Tk()
