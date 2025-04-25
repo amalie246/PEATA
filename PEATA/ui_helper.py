e15c5a8492ebf29abbba5bc2f89024cc83aebdf9
amalie246 <amni002@egms.no>
2025-04-25 15:36:26 +0200

    minor refactoring

--- PEATA/ui_helper.py
+++ PEATA/ui_helper.py
@@ -74,14 +74,9 @@ class UiHelper:
                 print(f"submitted data: {submitted_data}")
             
                 if len(submitted_data) == 2 and "AND" in submitted_data[0] and "username" in submitted_data[0] and "AND" in submitted_data[1] and "keyword" in submitted_data[1]:
-                    t1 = submitted_data[0]
-                    print("t1: ", t1)
-                    t2 = submitted_data[1]
-                    print("t2: ", t2)
+                    username = submitted_data[0]
+                    keyword = submitted_data[1]
                     
-                    
-                    username = t1[2]
-                    keyword = t2[2]
                     print("Hello usrname and keyword")
                     videos = self.tiktok_api.get_videos(username, keyword, start_date, end_date)
                     print(videos)
