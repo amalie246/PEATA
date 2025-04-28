6e1d33fc02271e96c62ef62cc48362c8f363e6bd
amalie246 <amni002@egms.no>
2025-04-28 19:11:17 +0200

    added exe

--- PEATA/config.py
+++ PEATA/config.py
@@ -5,9 +5,6 @@ JSON_FOLDER = os.path.join(BASE_FOLDER, "json")
 CSV_FOLDER = os.path.join(BASE_FOLDER, "csv")
 EXPORTS_FOLDER = os.path.join(BASE_FOLDER, "exports")
 
-# Ensure folders exist
 os.makedirs(JSON_FOLDER, exist_ok=True)
 os.makedirs(CSV_FOLDER, exist_ok=True)
 os.makedirs(EXPORTS_FOLDER, exist_ok=True)
-
-
